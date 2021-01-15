2eccf920985b827b8e0043a6383cf903fe9a6982
Zach Newton <znewton@microsoft.com>
2021-01-15 09:35:43 -0800

    parameterize method in stress http

--- packages/stress/src/http.ts
+++ packages/stress/src/http.ts
@@ -1,10 +1,51 @@
-import Axios from "axios";
+import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { stagger } from "@znewton/utils";
 import { ICommandResult } from "./commandHandler";
+
+export interface HttpResponse {
+    status: number;
+    statusText: string;
+    data: any;
+    isSuccess: boolean;
+}
+
+function makeHttpRequest(url: string, method?: AxiosRequestConfig["method"]): Promise<HttpResponse> {
+    const onSuccess = (response: AxiosResponse<any>): HttpResponse => {
+        return {
+            status: response.status,
+            statusText: response.statusText,
+            data: response.data,
+            isSuccess: true,
+        };
+    };
+    const onFailure = (reason: any): HttpResponse => {
+        if (reason?.response) {
+            const response: AxiosResponse<any> = reason.response;
+            return {
+                status: response.status,
+                statusText: response.statusText,
+                data: response.data,
+                isSuccess: false,
+            };
+        }
+        return {
+            status: -1,
+            statusText:
+                typeof reason === "string" || typeof reason?.toString === "function" ? reason.toString() : "Failed",
+            data: undefined,
+            isSuccess: false,
+        };
+    };
+    return Axios.request({
+        method,
+        url,
+    }).then(onSuccess, onFailure);
+}
 
 export async function sendMany(
     url: string,
     numRequests: number,
+    method: AxiosRequestConfig["method"] = "get",
     delayInMs = 0,
     verbose = false
 ): Promise<ICommandResult> {
@@ -13,27 +54,19 @@ export async function sendMany(
     const requestPromises: Promise<void>[] = [];
     await stagger(numRequests, delayInMs, () => {
         requestPromises.push(
-            Axios.get(url).then(
-                (response) => {
-                    if (verbose) {
-                        console.log(`🟢 ${response.status} ${response.statusText}`);
-                        console.log(response.data);
-                    }
-                    successCodes.push(response.status);
-                },
-                (reason) => {
-                    if (reason && reason.response) {
-                        if (verbose) {
-                            console.log(`🔴 ${reason.response.status} ${reason.response.statusText}`);
-                            console.log(reason.response.data);
-                        }
-                        failureCodes.push(reason.response.status);
-                    } else {
-                        console.error(reason || "Failed");
-                        failureCodes.push(-1);
-                    }
+            makeHttpRequest(url, method).then((httpResponse) => {
+                if (verbose) {
+                    console.log(
+                        `${httpResponse.isSuccess ? "🟢" : "🔴"} ${httpResponse.status} ${httpResponse.statusText}`
+                    );
+                    console.log(httpResponse.data);
                 }
-            )
+                if (httpResponse.isSuccess) {
+                    successCodes.push(httpResponse.status);
+                } else {
+                    failureCodes.push(httpResponse.status);
+                }
+            })
         );
     });
 
--- packages/stress/src/index.ts
+++ packages/stress/src/index.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestConfig } from "axios";
 import yargs from "yargs";
 import { commandHandler } from "./commandHandler";
 import * as http from "./http";
@@ -9,11 +10,20 @@ yargs
         {
             url: { type: "string", alias: "u", demandOption: true },
             numRequests: { type: "number", alias: "n", demandOption: false, default: 1 },
+            method: { type: "string", alias: "m", demandOption: false, default: "get" },
             delayInMs: { type: "number", alias: "d", demandOption: false, default: 0 },
             verbose: { type: "boolean", alias: "v", demandOption: false, default: false },
         },
         (argv) => {
-            commandHandler(async () => http.sendMany(argv.url, argv.numRequests, argv.delayInMs, argv.verbose))
+            commandHandler(async () =>
+                http.sendMany(
+                    argv.url,
+                    argv.numRequests,
+                    argv.method as AxiosRequestConfig["method"],
+                    argv.delayInMs,
+                    argv.verbose
+                )
+            )
                 .then(console.log)
                 .catch(console.error);
         }
